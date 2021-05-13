9d8d88b78446ea538d3c6aed38b4f1d43f56d765
Roland Levy <contact@rolandlevy.co.uk>
2021-05-13 18:22:01 +0000

    Initial commit

--- notes.md
+++ notes.md
@@ -2,3 +2,7 @@
 
 - https://www.youtube.com/watch?v=xn7A55xqFvw
 - https://essential-addons.com/elementor/docs/custom-js
+- Pools API [adapools.org/resources](https://adapools.org/resources)
+
+- fbclid: IwAR2HtMmcgedQR3PGMHSp3oXVwGtZsWUa6Lk4ycl7pYhOa9v2KkMZx7S2iOs
+- Crypto Justice Pool pool_id: 943fde2eb40fc5acdbc81c88230b56123231f83453eb72c023bc7707
--- script.js
+++ script.js
@@ -18,28 +18,59 @@ const maxValues = {
 
 let json;
 
-const fbclid = 'IwAR2HtMmcgedQR3PGMHSp3oXVwGtZsWUa6Lk4ycl7pYhOa9v2KkMZx7S2iOs';
+function displayProgressBars(pool_id) {
+  const poolUrl = `https://js.adapools.org/pools/${pool_id}/summary.json`;
+  fetch(poolUrl)
+  .then(res => res.json())
+  .then(result => {
+    json = {...result};
+    let str = '<ul>\n';
+    Object.entries(result.data).forEach(([key, value]) => {
+      str += `<li>${key} => ${value}</li>\n`;
+    });
+    str += '</ul>';
+
+    $('.db_name').textContent = result.data.db_name;
+    $('.db_url').href = result.data.db_url;
+
+    // $('#data-container').innerHTML = str;
+
+    Object.entries(maxValues).forEach(([key, max]) => {
+      const value = percentage(key, max) + '%';
+      updateProgressBar(key, value);
+    });
+
+  })
+  .catch(err => {
+    $('#data-container').innerHTML = `<p>${err}</p>`;
+  });
+}
+
 const pool_id = '943fde2eb40fc5acdbc81c88230b56123231f83453eb72c023bc7707';
+displayProgressBars(pool_id);
 
-const url = `https://js.adapools.org/pools/${pool_id}/summary.json?fbclid=${fbclid}`;
+const poolGroupsUrl = 'https://js.adapools.org/groups.json';
 
-fetch(url)
-.then(res => res.json())
-.then(result => {
-  json = {...result};
-  let str = '<ul>\n';
-  Object.entries(result.data).forEach(([key, value]) => {
-    str += `<li>${key} => ${value}</li>\n`;
+function generateDropdown() {
+  fetch(poolGroupsUrl)
+  .then(res => res.json())
+  .then(results => {
+    const groups = [...results];
+    // groups.sort((a, b) => a.group - b.group);
+    groups.sort((a, b) => {
+      return ((a.group < b.group) ? -1 : ((a.group > b.group) ? 1 : 0));
+    });
+    let str = '<select id="pool-group">\n';
+    groups.forEach(item => {
+      if (item.group) {
+        str += `<option value=${item.pool_id}>${item.group}</option>\n`;
+      }
+    });
+    str += '</select>';
+    $('.groups-list').innerHTML = str;
+    $('#pool-group').addEventListener('change', (e) => {
+      displayProgressBars(e.target.value);
+      $('.wrapper').classList.add('fade-in');
+    });
   });
-  str += '</ul>';
-  $('#data-container').innerHTML = str;
-
-  Object.entries(maxValues).forEach(([key, max]) => {
-    const value = percentage(key, max) + '%';
-    updateProgressBar(key, value);
-  });
-  
-})
-.catch(err => {
-  $('#data-container').innerHTML = `<p>${err}</p>`;
-});
+}
